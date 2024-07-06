070708b7c170afec8f2d3465b1f00c1644078ea7
Ben Nicholas <ben.nicholas96@gmail.com>
2024-07-06 09:24:36 +0100

    main

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def read_root():
 
 @app.get("/random")
 def random_number():
-    return random.randint(0, 6)
+    return random.randint(1, 6)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", port=8000, reload=True)
